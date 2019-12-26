9716e8eb7032c7e5401de8055f0f1ea30336d041
DanilchikB <danil1357997@gmail.com>
2019-12-26 20:26:36 +0300

    add pages: User, Login and Registration

---
+++ Application/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcUser.Controllers
+
+{
+    public class UserController : Controller
+    {
+        
+        // GET: /User/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        // GET: /User/Login/ 
+
+        public IActionResult Login ()
+        {
+            return View();
+        }
+
+        // GET: /User/Registration/
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
