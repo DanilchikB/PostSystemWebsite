d166078f540d84ee1fe1ce66fb56bfc007524745
DanilchikB <danil1357997@gmail.com>
2020-02-24 18:19:42 +0300

    add unit test for login controller

--- Application.Tests/UserControllerTests.cs
+++ Application.Tests/UserControllerTests.cs
@@ -15,6 +15,7 @@ namespace Application.Tests
 {
     public class UserControllerTests
     {
+        //Check for correct output index page
         [Fact]
         public async Task IndexTest()
         {
@@ -35,7 +36,7 @@ namespace Application.Tests
                 // Run the test against one instance of the context
                 using (var context = new DataContext(options))
                 {
-                    context.User.Add(new User { Id = 1, Username = "Bob", Password = "123456", Email = "bob@bob.bob"});
+                    GetTestUsers(context);
                     context.SaveChanges();
                 }
 
@@ -71,5 +72,74 @@ namespace Application.Tests
                 connection.Close();
             }
         }  
+        [Fact]
+        public async Task PostLoginTest(){
+            var connection = new SqliteConnection("DataSource=:memory:");
+                connection.Open();
+            try
+            {
+                var options = new DbContextOptionsBuilder<DataContext>()
+                    .UseSqlite(connection)
+                    .Options;
+                using (var context = new DataContext(options))
+                {
+                    context.Database.EnsureCreated();
+                }
+
+                using (var context = new DataContext(options))
+                {
+                    GetTestUsers(context);
+                    context.SaveChanges();
+                }
+
+                //check if errors are present
+                using (var context = new DataContext(options))
+                {
+                    var controller = new UserController(context);
+                    controller.ModelState.AddModelError("Email", "Required");
+                    User newUser = new User();
+
+                    var result = await controller.Login(newUser);
+
+
+                    var viewResult = Assert.IsType<ViewResult>(result);
+                    Assert.Equal(newUser, viewResult?.Model);
+                }
+                //check if errors are not present
+                using(var context = new DataContext(options)){
+                    var controller = new UserController(context);
+                    controller.ModelState.AddModelError("Email", "");
+                    controller.ModelState.AddModelError("Password", "");
+                    controller.ModelState["Email"].Errors.Clear();
+                    controller.ModelState["Password"].Errors.Clear();
+
+                    User newUser = new User(){
+                        Password = "123456", 
+                        Email = "bob@bob.bo"
+                    };
+
+                    var result = await controller.Login(newUser);
+
+                    
+                    var viewResult = Assert.IsType<ViewResult>(result);
+                    Assert.Equal("Wrong email or password", viewResult.ViewData["Error"]);
+                }
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+            
+        }
+        private void GetTestUsers(DataContext context)
+        {
+            context.User.Add(new User { Id = 1, Username = "Bob", Password = "123456", Email = "bob@bob.bob"});
+            context.User.Add(new User{ Id = 2, Username = "Alice", Password = "123456", Email = "alice@alice.alice"});
+            context.User.Add(new User { Id = 3, Username = "Misha", Password = "123456", Email = "misha@misha.misha"});
+            context.User.Add(new User { Id = 4, Username = "Mark", Password = "123456", Email = "mark@mark.mark"});
+                
+                
+        }
     }
 }
